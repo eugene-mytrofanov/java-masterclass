99d6a6816878cc08dda0a923b9cc14dcf0223cfc
Eugene <eugene.validation@gmail.com>
2020-02-09 13:43:02 +0200

    exceptions lesson

---
+++ src/net/mytrofanov/exceptions/Example.java
@@ -0,0 +1,44 @@
+package net.mytrofanov.exceptions;
+
+import java.util.InputMismatchException;
+import java.util.NoSuchElementException;
+import java.util.Scanner;
+
+public class Example {
+    public static void main(String[] args) {
+        try {
+            int result = divide();
+            System.out.println(result);
+        } catch (ArithmeticException | NoSuchElementException e) {
+            System.out.println(e.toString());
+            System.out.println("Unable to perform division, autopilot shutting down");
+        }
+    }
+
+    private static int divide() {
+        try {
+            int x, y;
+            x = getInt();
+            y = getInt();
+            System.out.println("x is " + x + ", y is " + y);
+            return x / y;
+        } catch (NoSuchElementException e) {
+            throw new ArithmeticException("no suitable input");
+        } catch (ArithmeticException e) {
+            throw new ArithmeticException("attempt to divide by zero");
+        }
+    }
+    private static int getInt() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please enter an integer");
+        while (true) {
+            try {
+                return sc.nextInt();
+            } catch (InputMismatchException e) {
+                // Go round again. Read past the end of line in the input first
+                sc.nextLine();
+                System.out.println("Please enter a number using only the digits 0-9sdfs");
+            }
+        }
+    }
+}
