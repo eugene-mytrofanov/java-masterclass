5ce464c3318c25ce86ade988c6e7294a8aa22344
Eugene <eugene.validation@gmail.com>
2020-02-17 22:59:43 +0200

    Add inheritance lesson

---
+++ src/net/mytrofanov/inheritance/Main.java
@@ -0,0 +1,11 @@
+package net.mytrofanov.inheritance;
+
+public class Main {
+    public static void main(String[] args) {
+        Animal animal = new Animal("Animal", 1, 1, 5, 25);
+        animal.eat();
+
+        Dog dog = new Dog("Haski", 4, 20, 2, 4, 1, 18, "Long fur");
+        dog.eat();
+    }
+}
