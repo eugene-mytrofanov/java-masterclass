b411303e29eeeacb47d8da4b3191c958a131befe
eugene.mytrofanov <eugene.mytrofanov@sigma.software>
2021-01-11 12:09:09 +0200

    Add ArrayList example

---
+++ src/net/mytrofanov/collection/list/arraylist/GroceryList.java
@@ -0,0 +1,39 @@
+package net.mytrofanov.collection.list.arraylist;
+
+import java.util.ArrayList;
+
+public class GroceryList {
+
+    // Incompleted exercise, should be finished
+
+    private ArrayList<String> groceryList = new ArrayList<>();
+
+    public void addGroceryItem(String item) {
+        groceryList.add(item);
+    }
+
+    public void printGroceryList() {
+        System.out.println("You have " + groceryList.size() + " items in your grocery list");
+        for(int i=0; i<groceryList.size(); i++) {
+            System.out.println((i+1) + ". " + groceryList.get(i));
+        }
+    }
+
+    public void modifyGroceryItem(int position, String newItem) {
+        groceryList.set(position, newItem);
+        System.out.println("Grocery item " + (position+1) + " has been modified");
+    }
+
+    public void removeGroceryItem(int position) {
+        String item = groceryList.get(position);
+        groceryList.remove(position);
+    }
+
+    public String findItem(String searchItem) {
+        int position = groceryList.indexOf(searchItem);
+        if(position >= 0) {
+            return groceryList.get(position);
+        }
+        return null;
+    }
+}
